c3d77bb2aa3ecdf17bee7c528baa5f1342b4b10a
JNA186506 <186506@stud.hvl.no>
2025-10-04 13:35:14 +0200

    Filopplastning

--- Forms/registrering.js
+++ Forms/registrering.js
@@ -1,5 +1,12 @@
 "use strict";
 
+const root = document.getElementById("root");
+const btn = root.querySelector("#btn");
+const sortBtn = root.querySelector("#sortBtn");
+const resetBtn = root.querySelector("#resetBtn");
+const filInput = root.querySelector("#opplastning")
+const lastOpp = root.querySelector("#lastOpp")
+
 class Deltager {
     startnummer;
     navn;
@@ -46,40 +53,37 @@ class DeltagerManager {
         }
     }
 
+    leggTilDeltagerITabellFraCSV(deltager) {
+        this.deltagerTabell.push(deltager);
+        this.deltagerTabell.sort((a, b) => a.sluttid.localeCompare(b.sluttid));
+
+        console.log(this.deltagerTabell);
+        this.renderTable(this.deltagerTabell);
+    }
+
     resetTable() {
-            this.tbody.innerHTML = "";
-            this.deltagerTabell.forEach( ((deltager, i) => {
+        this.tbody.innerHTML = "";
+        this.renderTable(this.deltagerTabell);
+    }
+
+    renderTable(tabell) {
+        this.tbody.innerHTML = "";
+
+        tabell.forEach(((deltager, i) => {
                 const row = document.createElement("tr");
                 row.innerHTML = `
-                <td>${i + 1}</td>
-                <td>${deltager.navn}</td>
-                <td>${deltager.startnummer}</td>
-                <td>${deltager.sluttid}</td>
-            `;
+                    <td>${i + 1}</td>
+                    <td>${deltager.navn}</td>
+                    <td>${deltager.startnummer}</td>
+                    <td>${deltager.sluttid}</td>
+                `;
                 this.tbody.appendChild(row)
             }));
             this.table.classList.remove("hidden");
 
             this.navn.value = "";
+            this.sluttid.value = "";
             this.startnummer.focus();
-    }
-
-    renderTable(tabell) {
-        this.tbody.innerHTML = "";
-        tabell.forEach( ((deltager, i) => {
-            const row = document.createElement("tr");
-            row.innerHTML = `
-                <td>${i + 1}</td>
-                <td>${deltager.navn}</td>
-                <td>${deltager.startnummer}</td>
-                <td>${deltager.sluttid}</td>
-            `;
-            this.tbody.appendChild(row)
-        }));
-        this.table.classList.remove("hidden");
-
-        this.navn.value = "";
-        this.startnummer.focus();
     }
 
     finnDeltagere() {
@@ -109,15 +113,49 @@ class DeltagerManager {
 
         return isValid;
     }
+
+    deltagereFraFil(stringVal, splitter) {
+        /* Den første consten vil ta en hel fin som tekst og splitte
+        * den opp i setninger, deretter splitter den opp setningene for hvert komma.
+        * Denne splitten tar for seg hver verdi lager en nøkkel altså startnummer, navn og sluttid.
+        * formedArr tar for seg mappen og lager en tabell av deltagere.*/
+        const [key, ...lines] = stringVal
+            .trim()
+            .split('\n')
+            .map((item) => item.split(splitter));
+
+        const formedArr = lines.map((line) => {
+            const object = {};
+            key.forEach((key, index) => {
+                object[key.trim()] = line[index].trim();
+            });
+
+            const parts = object.sluttid.split(":").map(p => p.padStart(2, "0"));
+            while (parts.length < 3) parts.push("00");
+            object.sluttid = parts.slice(0, 3).join(":");
+
+           return new Deltager(object.startnummer, object.navn, object.sluttid);
+        });
+
+        console.log(formedArr);
+        formedArr.forEach(deltager => this.leggTilDeltagerITabellFraCSV(deltager));
+        return formedArr;
+    }
 }
 
-
-const root = document.getElementById("root");
-const btn = root.querySelector("#btn");
-const sortBtn = root.querySelector("#sortBtn");
-const resetBtn = root.querySelector("#resetBtn");
-
 const deltagerManager = new DeltagerManager(root);
+
+lastOpp.addEventListener("click", function(event) {
+    const file = filInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        const csvArray = deltagerManager.deltagereFraFil(e.target.result, ",");
+    }
+    reader.readAsText(file);
+    filInput.value = "";
+    event.preventDefault();
+})
 
 btn.addEventListener("click", () => deltagerManager.leggTilDeltagerITabell());
 sortBtn.addEventListener("click", () => deltagerManager.finnDeltagere());
